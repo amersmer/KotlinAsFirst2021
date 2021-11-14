8390eb46b3377bba54984eb11d6556282984d17d
amersmer <n.e.grigore17@gmail.com>
2021-11-14 14:41:03 +0400

    14.11 1/2/3/4(1)

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -241,7 +241,19 @@ fun decimalFromString(str: String, base: Int): Int = TODO()
  * 90 = XC, 100 = C, 400 = CD, 500 = D, 900 = CM, 1000 = M.
  * Например: 23 = XXIII, 44 = XLIV, 100 = C
  */
-fun roman(n: Int): String = TODO()
+fun roman(n: Int): String {
+    var k = n
+    val a = listOf(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+    val r = listOf("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
+    var ans = ""
+    for (i in 0..12) {
+        for (j in 0 until k / a[i]) {
+            ans += r[i]
+            k %= a[i]
+        }
+    }
+    return ans
+}
 
 /**
  * Очень сложная (7 баллов)
